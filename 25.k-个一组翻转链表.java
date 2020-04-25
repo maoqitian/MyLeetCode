b2dc6599baf708f7e024ab53ae9c2845316c2047
maoqitian <maoqitian068@163.com>
2020-04-25 23:22:26 +0800

    update exercise

--- 25.k-个一组翻转链表.java
+++ 25.k-个一组翻转链表.java
@@ -45,16 +45,25 @@ class Solution {
         return result.next;
     }
 
-    //单转链表操作
+    //单转反转操作 原地反转
     private static ListNode reverse(ListNode pre, ListNode next){
+        // 获取当前节点，也就是反转之后最后一个节点
         ListNode last = pre.next;
+        //当前结点指向最后一个节点的后继
         ListNode cur = last.next;
-        while(cur != next){
+
+        //如果当前结点不等于反转组的最后一个节点
+        while(cur!= next){
+           //last连接下一次需要反转的节点
           last.next = cur.next;
-          cur.next = pre.next;
-          pre.next = cur;
-          cur = last.next;
+           //当前节点插入到头结点原本位置 反转节点cur
+           cur.next = pre.next;
+           //纠正头结点指向
+           pre.next = cur;
+           //当前节点继续指向下一个需要反转的节点
+           cur = last.next;
         }
+
         return last;
     }
 }
