37602cbe00cad5e067479c0a852f1983b9c36e7a
maoqitian <maoqitian068@163.com>
2020-05-21 13:25:10 +0800

    Update 433.最小基因变化.java

--- 433.最小基因变化.java
+++ 433.最小基因变化.java
@@ -9,52 +9,59 @@ import java.util.HashSet;
 // @lc code=start
 class Solution {
 
-    //想法 深度优先 递归 回溯
-    //最下步骤 声明
-    int minStepCount = Integer.MAX_VALUE;
+    //想法 DFS 递归 回溯
+
+    int minCount = Integer.MAX_VALUE;
+
     public int minMutation(String start, String end, String[] bank) {
-        
-        dfs(new HashSet<String>(),0,start,end,bank);
+        //使用 HashSet 存储基于组
 
-        return (minStepCount == Integer.MAX_VALUE)? -1 : minStepCount;
+        dfs(0,start,end,bank,new HashSet<String>());
+
+        return (minCount == Integer.MAX_VALUE) ? -1 : minCount;
+
     }
-
     /**
-     * 
-     * @param setp 存储基因组 集合 不可重复
-     * @param setpCount 当前次数
+     * @param setpCount  当前次数
      * @param currentStr 当前基于组
      * @param end 目标基于组
      * @param bank 基因库
+     * @param set 存储基因组 集合 不可重复
      */
-    void dfs(HashSet<String> setp,int setpCount,String currentStr,String end,String[] bank){
-       //递归结束条件
-       if(setpCount >= minStepCount){
-          return;
-       }
-       //逻辑处理 进入下层递归
-       //如果已经匹配到 更新次数
-       if(currentStr.equals(end)){
-          minStepCount = Math.min(setpCount, minStepCount);
-       }
-       //遍历基于库
-       for (String str : bank) {
-           int diff = 0;
-           for (int i = 0; i < str.length(); i++) {
-                  if(currentStr.charAt(i) != str.charAt(i)){
-                    diff++;
-                    if(diff > 1) break;
+    void dfs(int setpCount,String currentStr,String end,String[] bank,HashSet<String> set){
+
+        //递归结束条件
+        if(setpCount >= minCount){
+            //当前次数大于最小次数
+           return;
+        }
+        //逻辑处理 进入下层循环
+        if(currentStr.equals(end)){
+           //如果已经匹配到 获取最新次数
+           minCount = Math.min(minCount, setpCount);
+        }
+
+        //遍历基因库
+        for (String str : bank) {
+            int diff =0;
+            //循环判断当前字符是否有不同
+            for (int i = 0; i < str.length(); i++) {
+                if(currentStr.charAt(i) != str.charAt(i)){
+                  diff++;
+                  if(diff>1){ //如果有一个不同直接跳出循环
+                     break;
                   }
-                       
-           }
-           if(diff == 1 && !setp.contains(str)){
-            setp.add(str);
-            dfs(setp, setpCount+1, str, end, bank);
-            //当前层 数据 reverse
-            setp.remove(str);
-          }
-       }
+                }
+            }
+            if(diff == 1 && !set.contains(str)){//如果有不同,并且结合不含相同基因组
+                set.add(str);
+                //进入下层循环
+                dfs(setpCount+1, str, end, bank, set);
+                //回溯 数据 reverse
+                set.remove(str);
+            }
+            
+        }
     }
 }
 // @lc code=end
-
