acbfb46f2a84521fe62c5aa2d6f3adb7b5e15e22
maoqitian <maoqitian068@163.com>
2020-07-18 22:32:05 +0800

    add #35

---
+++ java/35.搜索插入位置.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=java
+ *
+ * [35] 搜索插入位置
+ */
+
+// @lc code=start
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+
+        //二分查找
+
+        int left = 0,right = nums.length;
+
+        while(left<right){
+            int mid = left+(right-left)/2;
+            if(nums[mid] >= target){
+                right = mid;
+            }else{
+                left = mid+1;
+            }
+        }
+
+         return left;
+    }
+}
+// @lc code=end
+
