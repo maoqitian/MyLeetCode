cc3b630d00d95a56aa63db2c2ed02b0f3c13d2c2
maoqitian <maoqitian068@163.com>
2020-07-28 13:16:55 +0800

    Update 104.二叉树的最大深度.java

--- java/104.二叉树的最大深度.java
+++ java/104.二叉树的最大深度.java
@@ -20,17 +20,41 @@ class Solution {
         //方法一 递归 +1 计算值 最深值 时间复杂度 O(n)
 
         //循环退出条件
-        if(root == null){
-            return 0;
+        // if(root == null){
+        //   return 0;
+        // }
+        // //逻辑处理调用下层循环
+        // return Math.max(maxDepth(root.left), maxDepth(root.right))+1;
+        // //数据释放
+
+         //广度优先遍历
+         //时间复杂度 O(n) 使用队列保存每一层的值
+         //遍历完一层深度加一
+         if(root == null){
+           return 0;
+         }
+
+        LinkedList<TreeNode> queue = new LinkedList<>();
+
+        queue.add(root);
+        //返回值结果深度
+        int ans = 0;
+        while(!queue.isEmpty()){
+             int sum = queue.size();
+             while(sum>0){
+               TreeNode node = queue.pop();
+                if(node.left!=null){
+                   queue.addLast(node.left);
+                }
+                if(node.right!=null){
+                  queue.addLast(node.right);
+               }
+               sum--;
+             }
+             ans ++;
         }
-        //逻辑处理 进入一层递归 
 
-        int leftHeight = maxDepth(root.left);
-
-        int rightHeight = maxDepth(root.right);
-        //返回最大值
-        return Math.max(leftHeight, rightHeight)+1;
-        //数据释放
+        return ans;
 
     }
 }
