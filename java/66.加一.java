ff4df80e1517ffdc52d5f878475416e111bdf647
maoqitian <maoqitian068@163.com>
2020-08-29 16:19:36 +0800

    add #214,update #66

--- java/66.加一.java
+++ java/66.加一.java
@@ -8,16 +8,21 @@
 class Solution {
     public int[] plusOne(int[] digits) {
 
-       //从末尾遍历 时间复杂度 O(n)
-       for (int i = digits.length-1; i>= 0; i--) {
-           digits[i]++;
-           digits[i] = digits[i]%10;
-           //如果不等于零 直接返回数组
-           if(digits[i]!=0) return digits;
-       }
-       digits = new int[digits.length + 1];
-       digits[0] = 1;
-       return digits;
+      //遍历相加 时间复杂度 O(n)
+
+      for(int i= digits.length -1;i>=0;i--){
+        //先加1
+        digits[i]++;
+        digits[i] = digits[i]%10;
+        if(digits[i] !=0){
+          //说明没有进位 直接返回数组
+          return digits;
+        }
+    }
+    //到此说明一直产生进位 则扩大数组长度并给第一位赋值1
+    digits = new int[digits.length+1];
+    digits[0] =1;
+    return digits;
 
     }
 }
