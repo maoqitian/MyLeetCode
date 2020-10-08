e1aa42324f528bd0114efc5daab952c6406c6856
maoqitian <maoqitian068@163.com>
2020-10-08 09:41:11 +0800

    add #344

---
+++ java/344.反转字符串.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=344 lang=java
+ *
+ * [344] 反转字符串
+ */
+
+// @lc code=start
+class Solution {
+    public void reverseString(char[] s) {
+
+        //双指针 时间复杂度 O(n)
+        int left = 0,right = s.length - 1;
+
+        while(left < right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
+// @lc code=end
+
