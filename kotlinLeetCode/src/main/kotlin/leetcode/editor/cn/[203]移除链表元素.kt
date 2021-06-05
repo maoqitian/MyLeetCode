2fbe58db5057e3794e1eda940b3bf39e4618ae84
maoqitian <maoqitian068@163.com>
2021-06-05 19:22:20 +0800

    add #203

---
+++ kotlinLeetCode/src/main/kotlin/leetcode/editor/cn/[203]移除链表元素.kt
@@ -0,0 +1,70 @@
+//给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,6,3,4,5,6], val = 6
+//输出：[1,2,3,4,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [], val = 1
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [7,7,7,7], val = 7
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 列表中的节点在范围 [0, 104] 内 
+// 1 <= Node.val <= 50 
+// 0 <= k <= 50 
+// 
+// Related Topics 链表 
+// 👍 621 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun removeElements(head: ListNode?, `val`: Int): ListNode? {
+
+        val res = ListNode(0)
+        res.next = head
+        //记录当前的节点值
+        var temp = res
+
+        while (temp.next != null){
+
+            if (temp.next.`val` == `val`){
+                //跳过当前节点
+                temp.next = temp.next.next
+            }else{
+                temp = temp.next
+            }
+        }
+
+        return res.next
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
